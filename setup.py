0233583121425e3ffdfdef8bcea553e409d126b4
Andrew Straw <strawman@astraw.com>
2009-02-01 11:32:31 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='remote_traits',
+      version='0.0.1',
+      license='BSD',
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
+      url='http://github.com/astraw/remote_traits',
+      py_modules=['remote_traits'],
+      )
