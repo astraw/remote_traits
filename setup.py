4e5cb246f74c8391aa2a1773459065bd88e73d1c
Andrew Straw <strawman@astraw.com>
2009-02-01 11:45:37 -0800

    correct license

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='remote_traits',
       version='0.0.1',
-      license='BSD',
+      license='MIT',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       url='http://github.com/astraw/remote_traits',
