541ff5a865ba8ee3805d3a8c8c777d9100fa50b5
Andrew Straw <strawman@astraw.com>
2009-02-15 02:03:28 -0800

    bump version for release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='remote_traits',
-      version='0.0.1',
+      version='0.0.2',
       license='MIT',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
