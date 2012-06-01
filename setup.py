6408befdc082b1fe1e401ce185c5c056283301f4
Andrew Straw <strawman@astraw.com>
2012-06-01 11:30:00 +0200

    bump version for release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='remote_traits',
-      version='0.0.2+git',
+      version='0.1',
       description='use traits remotely',
       license='MIT',
       author='Andrew Straw',
